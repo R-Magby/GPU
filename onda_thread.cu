6d89dcb37066f837be73da67329993554d8c07fd
RODOLFO <115359072+R-Magby@users.noreply.github.com>
2023-09-14 10:10:55 -0300

    Update onda_thread.cu

--- onda_thread.cu
+++ onda_thread.cu
@@ -5,25 +5,22 @@
 
 
 __global__ void onda2d(float *u, float *u_m1, float *u_p1,const float dt,const float dx,const float c,const int T) {
-    int x = threadIdx.x ;
-    int y = threadIdx.y ;
-    int Nx = blockDim.x ;
-    int Ny = blockDim.y ;
-    __shared__ float temp[32*32];
+    int x = threadIdx.x + blockDim.x*blockIdx.x;
+    int y = threadIdx.y + blockDim.y*blockIdx.y;
+    int Nx = gridDim.x*blockDim.x ;
+    int Ny = gridDim.y*blockDim.y ;
 
     if (x>0 && x<Nx-1 && y>0 && y<Ny-1){
-        temp[y*Ny+x]=u[y*Ny+x];
 
         for (int t=0;t<T;t++){
-            
-            u_p1[y*Ny+x]=dt*dt/(dx*dx)*c*c*(temp[y*Ny+x+1]+temp[y*Ny+x-1]+
-                temp[(y+1)*Ny+x]+temp[(y-1)*Ny+x]-4.0*temp[y*Ny+x]) + 2.0*temp[y*Ny+x]-u_m1[y*Ny+x];
+
+            u_p1[y*Ny+x]=dt*dt/(dx*dx)*c*c*(u[t*Nx*Ny+y*Ny+x+1]+u[t*Nx*Ny+y*Ny+x-1]+
+                u[t*Nx*Ny+(y+1)*Ny+x]+u[t*Nx*Ny+(y-1)*Ny+x]-4.0*u[t*Nx*Ny+y*Ny+x]) + 2.0*u[t*Nx*Ny+y*Ny+x] - u_m1[y*Ny+x];
             __syncthreads();
 
-            u_m1[y*Ny+x]=temp[y*Ny+x];
-        
-            temp[y*Ny+x]=u_p1[y*Ny+x];
-            u[t*Nx*Ny+y*Ny+x]=temp[y*Ny+x];
+            u_m1[y*Ny+x]=u[t*Nx*Ny+y*Ny+x];
+
+            u[(t+1)*Nx*Ny+y*Ny+x]=u_p1[y*Ny+x];
             __syncthreads();
 
         }
@@ -86,8 +83,10 @@ int main(int argc, char *argv[]){
     cudaMemcpy(d_u_m1, u_m1, size, cudaMemcpyHostToDevice);
 
     // Invocar kernel con N bloques de 1 thread cada uno
-    dim3 bloque (Nx,Ny);
-    onda2d<<<1,bloque>>>(d_u,d_u_m1,d_u_p1,dt,dx,c,T);
+    dim3 bloque (32,32);
+    dim3 grid(ceil(Nx/32),ceil(Ny/32));
+    printf("%d %d",(int)ceil(Nx/32),(int)ceil(Ny/32));
+    onda2d<<<grid,bloque>>>(d_u,d_u_m1,d_u_p1,dt,dx,c,T);
     
     cudaError_t err = cudaGetLastError();
     printf("Error: %s\n",cudaGetErrorString(err));
